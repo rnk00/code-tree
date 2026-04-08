c6d1cbe5d228a0f09c9c2f48e9d8d9261ad9b9b1
ryu namkyong <139599074+rnk00@users.noreply.github.com>
2026-04-08 17:53:52 +0900

    Title: 계단 오르기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260408/계단 오르기/climbing-stairs.java
+++ 260408/계단 오르기/climbing-stairs.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         int[] dp = new int[1005];
+        dp[0] = 1;
         dp[1] = 0;
         dp[2] = 1;
         dp[3] = 1;

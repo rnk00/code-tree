4a9eb7eb5288a9a781c602b385b555b7422d355e
ryu namkyong <139599074+rnk00@users.noreply.github.com>
2026-04-08 17:54:36 +0900

    Title: 계단 오르기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260408/계단 오르기/climbing-stairs.java
+++ 260408/계단 오르기/climbing-stairs.java
@@ -9,12 +9,12 @@ public class Main {
         dp[2] = 1;
         dp[3] = 1;
         for(int i=4; i<=1000; i++){
-            dp[i] = dp[i-2] + dp[i-3];
+            dp[i] = (dp[i-2] + dp[i-3]) % 10007;
         }
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine().trim());
 
-        System.out.println(dp[N] % 10007);
+        System.out.println(dp[N]);
     }
 }

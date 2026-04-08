e6711dc9bb14b1ea120a9105ed91efc6e82703ea
ryu namkyong <139599074+rnk00@users.noreply.github.com>
2026-04-08 17:49:30 +0900

    Title: 계단 오르기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260408/계단 오르기/climbing-stairs.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int[] dp = new int[1005];
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+        for(int i=4; i<=1000; i++){
+            dp[i] = dp[i-2] + dp[i-3];
+        }
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine().trim());
+
+        System.out.println(dp[N]);
+    }
+}
